dbd348420a3df4986dde72da3f6ac5a35c5a67b8
Roushan <you@example.com>
2017-07-07 11:48:49 +0530

    New Routers

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.static(path.join(__dirname, ('public'))));
+app.use(express.static(path.join(__dirname, ('semantic/dist'))));
 
 //Middleware 
 
