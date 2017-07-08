6b067f1fe912fe7e58e01ee4e6c7c2c87e282cd6
Roushan <you@example.com>
2017-07-08 19:07:05 +0530

    changes routes

--- router/home.js
+++ router/home.js
@@ -6,24 +6,10 @@ router.get('/', function(req, res, next) {
 	res.render('home');
 });
 
-router.get('/user', function(req, res, next) {
 
-	res.render('user');
-});
 
-router.get('/login', function(req, res, next) {
 
-	res.render('login');
-});
 
-router.get('/logout', function(req, res, next) {
 
-	res.render('logout');
-});
-
-router.get('/registration', function(req, res, next) {
-
-	res.render('registration');
-});
 
 module.exports = router;
