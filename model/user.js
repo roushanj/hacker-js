a37b49bcd5fe7290a269a08abf9ba32124684d16
Roushan <you@example.com>
2017-07-08 19:04:58 +0530

    create schema for user

---
+++ model/user.js
@@ -0,0 +1,47 @@
+var mongoose  = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+var UserSchema = mongoose.Schema({
+
+	username:{
+		type: String,
+		index:true
+	},
+	name:{
+		type: String,
+		index:true
+	},
+	email:{
+		type: String,
+		index:true
+	},
+	password:{
+		type: String,
+		index:true
+	}
+});
+
+var User = module.exports = mongoose.model('User',UserSchema);
+
+module.exports.createUser = function(newUser, callback) {
+         
+         newUser.save(callback);
+}
+
+module.exports.getUserByUsername = function (username, callback) {
+	var query = {username: username};
+	User.findOne(query, callback); 
+}
+
+module.exports.getUserById = function (id, callback) {
+	
+	User.findById(id, callback); 
+}
+
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+	bcrypt.compare(candidatePassword, function (err, isMatch) {
+
+		if (err) throw err;
+		callback(null, isMatch);
+	});
+}
